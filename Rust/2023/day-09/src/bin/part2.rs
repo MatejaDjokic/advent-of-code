2f50f1c412c7d197129d2aa03295c9b5daee4ed9
_p4lm4 <matejadjokic23@gmail.com>
2023-12-26 01:07:20 +0100

    day-09: part 1 succes 1/2 part 2 wip

---
+++ Rust/2023/day-09/src/bin/part2.rs
@@ -0,0 +1,80 @@
+fn main() {
+    let input: &str = include_str!("./input1.txt");
+    let output = process(input);
+    dbg!(output);
+}
+
+fn process(input: &str) -> i128 {
+    let lines = input.lines().map(|l| l.trim()).collect::<Vec<_>>();
+
+    let mut sums: Vec<i128> = Vec::new();
+    for line in lines[0..1].iter() {
+        let values = line
+            .split(" ")
+            .map(|c| c.parse().unwrap())
+            .collect::<Vec<i128>>();
+
+        let mut dataset: Vec<Vec<i128>> = vec![values.clone()];
+
+        while !dataset.last().unwrap().iter().all(|n| *n == 0) {
+            let last = dataset.last().unwrap();
+            let new_values = last
+                .windows(2)
+                .map(|pair| pair[1] - pair[0])
+                .collect::<Vec<i128>>();
+            dataset.push(new_values);
+        }
+
+        let sum = dataset
+            .iter()
+            .map(|d1| *d1.first().ok_or(format!("Length: {}", d1.len())).unwrap())
+            .collect::<Vec<_>>()
+            .iter()
+            .fold(0, |u1, u2| u2 - u1);
+
+        // println!(
+        //     "{}",
+        //     sum.iter()
+        //         .map(|f| f.to_string())
+        //         .collect::<Vec<_>>()
+        //         .join(", ")
+        // );
+
+        printds(dataset);
+        println!("{}", sum);
+        // println!("{}", sum.iter().fold(0, |u1, u2| u2 - u1));
+        sums.push(sum);
+    }
+
+    sums.iter().fold(0, |s1, s2| s1 + s2)
+}
+
+fn printds(ds: Vec<Vec<i128>>) {
+    let temp = ds
+        .iter()
+        .map(|d1| {
+            d1.iter()
+                .map(|d2| d2.to_string())
+                .collect::<Vec<String>>()
+                .join(", ")
+        })
+        .collect::<Vec<String>>()
+        .join("\n");
+
+    println!("{}", temp);
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    //     #[test]
+    //     fn test1() {
+    //         let result = process(
+    //             "0 3 6 9 12 15
+    // 1 3 6 10 15 21
+    // 10 13 16 21 30 45",
+    //         );
+    //         assert_eq!(result, 2);
+    //     }
+}
